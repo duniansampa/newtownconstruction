98689aa42b741983a7b3cf50a5572b7f4b4fd9ed
Dunian Coutinho Sampa <duniansampa@outlook.com>
2022-05-06 20:44:59 -0700

    changing relative link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,7 +24,7 @@ export default function Header() {
         <div className="collapse navbar-collapse" id="navbar-items">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#navbar">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
